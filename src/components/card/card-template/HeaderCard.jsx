8e6c864b7b9de0d2e5414625ebc36c14265a6b15
ausubel <ausubelvc@gmail.com>
2024-12-14 10:42:39 -0500

    prueba

--- src/components/card/card-template/HeaderCard.jsx
+++ src/components/card/card-template/HeaderCard.jsx
@@ -1,11 +1,2 @@
 import { Container, CardTitle } from "./styles";
 
-const HeaderCard = ({ cardName }) => {
-    return (
-        <Container>
-            <CardTitle>{cardName}</CardTitle>
-        </Container>
-    );
-};
-
-export default HeaderCard;
